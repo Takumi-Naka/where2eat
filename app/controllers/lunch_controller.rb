6d4fa165ef264185ff184458a7638a54ac3fc6fd
Takumi-Naka <notenmarks0123@gmail.com>
2019-05-10 17:23:54 +0900

    基本情報を追加

--- app/controllers/lunch_controller.rb
+++ app/controllers/lunch_controller.rb
@@ -1,6 +1,6 @@
 class LunchController < ApplicationController
     def go
-	    eataries = ['松のや','あすかて食堂','te.to.te','トタンコットンカフェ','711で弁当']
+            eataries = Eatery.all	    
 
 	    @recommend = eataries.sample    
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Eatery.delete_all
+Eatery.create(name: "松のや", address: "茨城県つくば市天久保4丁目6-8(〒305-0005)", tel: "029-850-0781", regular_holiday: "なし", open: "6:00", close: "2:00")
+Eatery.create(name: "あすかて食堂", address: "茨城県つくば市天久保2丁目10-2(〒305-0005)", tel: "なし", regular_holiday: "不定休", open: "11:30", close: "22:00")
